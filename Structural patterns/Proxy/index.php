1043e4dff5b76603aa6395dd4bfb9895fca6fb0f
Daniel S <sech-daniil@yandex.ru>
2023-08-16 10:02:54 +0300

    add new pattern proxy

---
+++ Structural patterns/Proxy/index.php
@@ -0,0 +1,50 @@
+<?php
+
+interface Worker
+{
+    public function closedHour($hours);
+
+    public function countSalary(): int;
+}
+
+class WorkerOutsource implements Worker
+{
+    private array $hours = [];
+    public function closedHour($hours)
+    {
+        $this->hours[] = $hours;
+    }
+
+    public function countSalary(): int
+    {
+        return array_sum($this->hours) * 20;
+    }
+}
+
+class WorkerProxy extends WorkerOutsource implements Worker
+{
+    private int $salary = 0;
+
+    public function countSalary(): int
+    {
+        if ($this->salary === 0) {
+            // Считает только в том случае если у нас 0.
+            // Дальше этот метод дёргаться не будет.
+            $this->salary = parent::countSalary();
+        }
+
+        return $this->salary;
+    }
+}
+
+$workerProxy = new WorkerProxy();
+$workerProxy->closedHour(10);
+$salary = $workerProxy->countSalary();
+
+$workerProxy->closedHour(2000);
+$workerProxy->closedHour(3990);
+$workerProxy->closedHour(4000);
+
+$salary = $workerProxy->countSalary();
+
+var_dump($salary); // Output: 400
